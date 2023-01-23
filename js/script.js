3868bb27c43817a68d7fa3c6a5b9f70a245fed11
mgruppo <99413368+mgruppo@users.noreply.github.com>
2023-01-23 17:47:44 -0300

    se agrega manejo de array en tasa de inversion, para agregar o eliminar una tasa de financiamiento, se agregan comentarios de opciones que deben modificarse y trabajar, es necesario continuar trabajando con la simulación y el informe final

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ al igual que el fondo comun de Mercado Libre, se deberia tomar de algun servicio
 const tasaPlazoFijo = 0.75; //tasa de plazo fijo (Tasa Nominal Anual)
 const tasaFondoComun = 0.62; //tasa de fondo comun (Tasa Nominal Anual)
 const mensajeSalida = "salir" //para salir de la ejecucion
-const planes = [];
+const planes = []; //array de planes para que en proxima version pueda tener varios planes
+
 //agrego los días para comparar, los días sabado y domingo no se permite realizar inversiones
 const dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
 
@@ -16,12 +17,10 @@ let tasaTarjeta6 = 0.15;
 let tasaTarjeta12 = 0.24;
 
 //muestro las opciones de inversion
-const tasasInversion = [{
-    id: 1,
+let tasasInversion = [{
     descripcion: "Plazo Fijo",
     valor: 0.75
 }, {
-    id: 2,
     descripcion: "Fondo Común",
     valor: 0.62
 }];
@@ -42,6 +41,18 @@ const tasasTarjetas = [{
     descripcion: "12 Cuotas",
     cantidad: 12,
     valor: 0.24
+}];
+
+//ver si tiene sentido o se borra el proposito
+const proposito = [{
+    id: 1,
+    tipo: "Viaje"
+}, {
+    id: 2,
+    tipo: "Electronica"
+}, {
+    id: 3,
+    tipo: "Ropa"
 }];
 
 //variables de eleccion de viaje
@@ -72,15 +83,16 @@ let valorInteres = (opcion) => {
     }
 }
 //como el calculo de interes es igual, se utiliza para gasto, como para ganancia
-function interes(importe, interes, cantidadMeses) {
+//estaria bueno incluir el calculo mensual en un arrays en plan
+let interes = (importe, interes, cantidadMeses) => {
     let valorResultado = 0
     for (let i = 1; i <= cantidadMeses; i++) {
-        valorResultado = valorResultado + (importe * (interes / 12))
+        valorResultado += (importe * (interes / 12))
     }
     return valorResultado
 }
 
-function calcular(cantidadCuotas, montoGasto) {
+let calcular = (cantidadCuotas, montoGasto) => {
     /*la idea es que: no importa las cuotas que seleccione el usuario
     pero en base a las opciones, mostrarle al usuario las ventajas de invertir el dimero un plazo fijo
     o en un fondo común de inversión y con el valor de los intereses pagar la cuota.
@@ -108,22 +120,115 @@ function calcular(cantidadCuotas, montoGasto) {
     //puede ser una funcion generar presupuesto o informe
 
     let mensajeFinal = `Elegiste pagar en ` + eleccionCuotas + `, pero sabias que pagando en ` + `\n 1 cuota, el gasto te saldría 1 desembolso unico de $` + montoGasto + `\n En 6 cuotas, te saldría $` + valorCuotas6 + ` y podrías descontar los intereses de un Fondo Común de Inversión $` + valorFondoComun6 + ` o los intereses de un Plazo Fijo $` + valorPlazoFijo6 + ` que podrias descontar en las cuotas mensualmente reinvirtiendo el capital únicamente` + `\n En 12 cuotas, te saldría $` + valorCuotas12 + ` y podrías descontar los intereses de un Fondo Común de Inversión $` + valorFondoComun12 + ` o los intereses de un Plazo Fijo $` + valorPlazoFijo12 + ` que podrias descontar en las cuotas mensualmente reinvirtiendo el capital únicamente`
-    alert(mensajeFinal)
-    console.log(mensajeFinal)
-
+    //imprimo informe final
     informe(mensajeFinal);
 }
 
-function informe(mensajeFinal) {
-    
-
-    
+let informe = (mensajeFinal) => {
     alert(mensajeFinal)
     console.log(mensajeFinal)
-
-
 }
 
+//AGREGAR
+//funciones de orden superior
+//podria utilizar para calcular si la ganancia mensual es mayor que la inflacion
+function mayorQue(n) {
+    return (m) => m > n
+}
+
+//si con la ganancia de los intereses le gano a la inflacion mensual
+let inflacion = 0.4
+let mayorQueInflacion = mayorQue(inflacion)
+
+//recibir funciones por parametro
+//por cada uno de los calculos del array valido si es mayor a la inflacion
+//puedo mostrar cuando le gano o no a la inflacion
+function porCadaUno(arr, fn) {
+    for (const iterator of arr) {
+        /*for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+        }*/
+        fn(iterator)
+    }
+}
+
+
+//podria ser, la funcion no lleva parentesis
+//porCadaUno(array,mayorQueInflacion)
+//porCadaUno(numeros, console.log)
+let total = 0
+
+function acumular(num) {
+    total += num
+}
+numero = [1, 2, 3, 4, 5]
+porCadaUno(numero, acumular)
+//console.log(total)
+
+const duplicado = []
+porCadaUno(numero, (el) => {
+    duplicado.push(el * 2)
+})
+//console.log(duplicado)
+
+//for each
+// tasasInversion.forEach((tasa) => {
+//     console.log(tasa.descripcion)
+// })
+
+//find para buscar, recorre y cuando encuentra uno corta, no recorre todo
+const resultadoInflacion = planes.find((plan) => plan.descripcion == "eeuu")
+//console.log("resultadoInflacion " + resultadoInflacion)
+
+//filter me devuelve un nuevo array
+const resultado2 = planes.filter((plan) => plan.activo == true)
+//console.log(resultado2)
+
+//some es igual a filter pero devuelve true o false
+//console.log("some " + planes.some((plan) => plan.activo == true))
+
+//map me devuelve un nuevo array con los elementos del original pero con la transformacion
+const resultado3 = planes.map((plan) => plan.descripcion)
+//console.log(resultado3)
+
+//reduce, me devuelve un unico valor, no se utiliza tanto
+numero = [1, 2, 3, 4, 5]
+const resultado4 = numero.reduce((acumulador, elemento) => acumulador + elemento, 0)
+//console.log("reduce " + resultado4)
+
+//sort para ordenar un array, el metodo es destructivo, luego del orden cambia el lugar
+numero = [40, 1, 5, 200]
+numero.sort((a, b) => a - b); //ascendente
+numero.sort((a, b) => b - a); //descendente
+//console.log("sort  " + numero.sort((a, b) => a - b))
+//console.log("sort  " + numero.sort((a, b) => b - a))
+
+//ordenar por descripcion
+tasasTarjetas.sort((a, b) => {
+    if (a.descripcion > b.descripcion) {
+        return 1;
+    } else if (a.descripcion < b.descripcion) {
+        return -1;
+    } else {
+        return 0;
+    }
+})
+//console.log(tasasTarjetas.toString())
+
+//Math, son metodos matematicos para trabajar en javascript
+/*
+console.log("Math")
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.round(Math.PI));
+*/
+
+//Date
+const hoy = new Date();
+/*
+console.log(new Date())
+console.log(hoy.getDay()) //1 es lunes y 7 domingo
+*/
 //creo clase para definir el plan
 class Plan {
     //nombre del plan
@@ -136,8 +241,17 @@ class Plan {
         this.monto = parseFloat(monto);
         this.financiacion = financiacion;
         this.activo = true;
+        //agregar un arrays con el detalle de la financiacion, mes, interes, ganancia, pago cuota
+        //esto se va a poder ordenar y visualizar entre ellos
+        this.detalleFinanciacion = {
+            mesNumero: 0,
+            interesTarjeta: 0,
+            interesInversion: 0,
+            valorTotal: 0
+        };
+
     }
-    //agrego un metodo
+    //agrego un metodo de activo, para que cuando tenga varios planes, poder simular entre los activos
     cambiarActivo(activo) {
         this.activo = activo;
         return this.activo
@@ -148,6 +262,91 @@ class Plan {
     }
 }
 
+//for para mostrar los valores por defecto de las tasas
+let tasaMensaje = (tasaReferencia) => {
+    let mensajeAuxiliar = ""
+    for (let index = 0; index < tasaReferencia.length; index++) {
+        if (mensajeAuxiliar.trim() == "") {
+            mensajeAuxiliar += (tasaReferencia[index].descripcion + " " + (tasaReferencia[index].valor * 100) + "%")
+        } else {
+            mensajeAuxiliar += ("\n" + tasaReferencia[index].descripcion + " " + (tasaReferencia[index].valor * 100) + "%")
+        }
+    }
+    return mensajeAuxiliar
+}
+
+let nuevaTasa = (tasaReferencia) => {
+    do {
+        nuevaTasaPorcentaje = Number(prompt(`Por favor, en el rango de 0 (cero) a 100(cien), ingrese el valor de la tasa de refencia, correspondiente a la Tasa Nominal Anual (TNA)`));
+    } while (!((nuevaTasaPorcentaje / 100) >= 0 && (nuevaTasaPorcentaje / 100) <= 1));
+    do {
+        nuevaTasaDescripcion = prompt(`Por favor, ingrese el nombre que identifica a la tasa de refencia`);
+    } while (nuevaTasaDescripcion.trim() == "");
+    tasaReferencia.push({
+        descripcion: nuevaTasaDescripcion,
+        valor: (nuevaTasaPorcentaje / 100)
+    })
+    //alert("Pefecto, la nueva tasa se agrego correctamente")
+}
+
+let eliminarTasa = (tasaReferencia) => {
+    let mensajeEleccion = tasaMensaje(tasaReferencia)
+    let contadorBorrado = 0
+    do {
+        tasaEliminar = prompt(`Por favor, de las siguientes tasas:\n ${mensajeEleccion} \nIngrese el nombre de la tasa a eliminar, \nDe lo contrario, escriba ${mensajeSalida} para abandonar el borrado y continuar`);
+    } while (tasaEliminar.trim() == "");
+
+    if (tasaEliminar.toLowerCase() != mensajeSalida) {
+        //let index = tasaReferencia.indexOf({descripcion: tasaEliminar.trim()})
+        index = tasaReferencia.findIndex(i => i.descripcion === tasaEliminar)
+        //si existe, o sea, es distinto a -1, lo borro con splice
+        if (index != -1) {
+            tasaReferencia.splice(index, 1)
+            contadorBorrado++
+        }
+        //alert("Perfecto, la tasa se borro correctamente")
+    }
+
+    if (contadorBorrado > 0) {
+        alert("Perfecto, la tasa se borro correctamente")
+    } else {
+        alert("No se encontró la tasa a borrar")
+    }
+
+    // mensajeEleccion = tasaMensaje(tasaReferencia)
+    // alert(`Continuamos con las siguientes tasas:\n ${mensajeEleccion}`)
+    return tasaReferencia
+}
+
+let controlTasaInversion = (tasasInversion) => {
+    let mensajeEleccion = tasaMensaje(tasasInversion)
+    let control = false;
+    do {
+        eleccionTasa = Number(prompt(`Para la siguiente simulación, vamos a trabajar con los siguientes valores de inversión\n${mensajeEleccion}\nEscriba\n1 - Para continuar\n2 - Para agregar una nueva tasa al listado\n3 - Para eliminiar una tasa del listado anterior`));
+        switch (eleccionTasa) {
+            case 1:
+                //continuo con las dos tasas ingresadas por defecto
+                control = true;
+                break;
+            case 2:
+                //agrego una nueva tasa
+                control = true;
+                nuevaTasa(tasasInversion);
+                break;
+            case 3:
+                //elimino una de las tasas - si o si, debe existir una tasa, para la simulacion
+                control = true;
+                tasasInversion = eliminarTasa(tasasInversion);
+                break;
+            default:
+                //opcion incorrecta, debe ingresar un valor correcto
+                break;
+        }
+    } while ((control == false));
+    mensajeEleccion = tasaMensaje(tasasInversion)
+    alert(`Continuamos con las siguientes tasas:\n ${mensajeEleccion}`)
+}
+
 
 //logica de codigo
 //saludo
@@ -155,18 +354,16 @@ alert("Bienvenido a la plataforma de decisión financiera");
 //solicitud de nombre, para pertenencia en el resultado
 //validacion de nombre de la persona con do while
 
-
-
 do {
     nombre = prompt(`Por favor, ingresa tu nombre para continuar \nEscriba ${mensajeSalida} para abandonar el programa`);
-    if (nombre == "") {
+    if (nombre.trim() == "") {
         alert(`Por favor, ingresa un nombre válido para continuar \nEscriba ${mensajeSalida} para abandonar el programa`);
     } else if (nombre.toLowerCase() == mensajeSalida) {
         //para salir de la condicion
         break
     }
-} while (nombre == "");
-if ((nombre != "") && (nombre.toLowerCase() != mensajeSalida)) {
+} while (nombre.trim() == "");
+if ((nombre.trim() != "") && (nombre.toLowerCase() != mensajeSalida)) {
     console.log("nombre" + " " + nombre);
     //casteo el numero de la eleccion, por defecto es el valor 0 (sin eleccion) y se asigna nombre otros, de lo contrario se establece entre las opciones
     eleccion = Number(prompt("Contanos " + nombre + ", ¿cuál es tu deseo financiero? Ingresa el número de la opción para continuar \nIngresa 1 para Viaje, \nIngresa 2 para Electronica, \nIngresa 3 para Ropa, \nIngresa 4 para Otro"));
@@ -190,9 +387,9 @@ if ((nombre != "") && (nombre.toLowerCase() != mensajeSalida)) {
     }
     if (eleccion >= 1 && eleccion <= 4) {
         //definir un nombre del plan para hacerlo mas personalizado
-        nombrePlan = prompt(`1 - Necesitamos un poco más de información ¿Cómo llamarías a tu plan de la categoría: ${eleccionLetras}?`);
-        while (nombrePlan == "") {
-            nombrePlan = prompt(`2 - Necesitamos un poco más de información ¿Cómo llamarías a tu plan de la categoría: ${eleccionLetras}?`);
+        nombrePlan = prompt(`Necesitamos un poco más de información ¿Cómo llamarías a tu plan de la categoría: ${eleccionLetras}?`);
+        while (nombrePlan.trim() == "") {
+            nombrePlan = prompt(`Necesitamos un poco más de información ¿Cómo llamarías a tu plan de la categoría: ${eleccionLetras}?`);
             console.log("nombrePlan ", nombrePlan)
         }
         //validacion de monto con do while, por defecto el programa funcionara si el valor es entre 1000 y 2 millones, de lo contrario no continua
@@ -228,18 +425,22 @@ if ((nombre != "") && (nombre.toLowerCase() != mensajeSalida)) {
                 }
                 while (cantidadCorrecta == false);
                 console.log(eleccionCuotas, " - ", cantidadCorrecta)
-                alert("Perfecto, vamos a ayudarte en tu plan " + eleccionLetras + " por un total de " + montoGasto.toString() + " y en " + eleccionCuotas);
+                alert("Perfecto, vamos a ayudarte en tu plan " + eleccionLetras + " de nombre " + nombrePlan + " por un total de " + montoGasto.toString() + " y en " + eleccionCuotas);
 
                 //cargo la informacion y creo el plan, podría crear varios, estilo carrito de compras y calcular varios planes y un plan general.
                 planes.push(new Plan(nombre, eleccion, montoGasto, cantidadCuotas));
-                console.log(planes)
-                
+                console.log("Imprimimos los planes")
+                console.log(planes);
+                console.log("Imprimimos los planes con toString")
                 planes.toString();
 
                 for (let index = 0; index < planes.length; index++) {
                     //alert (numeros[index]);
                     console.log(planes[index]);
                 }
+
+                //Para controlar si el usuario desea agregar una nueva tasa o eliminar otra
+                controlTasaInversion(tasasInversion);
 
                 //controlar
                 // for(const plan1 of plan){
